6450be438fd0f51239756fc98d7b8ce41ec33b3e
shimazaki-ryuta <k022g0024@g.neec.ac.jp>
2023-04-19 14:29:13 +0900

    01_00 スワップチェーンの生成まで

--- main.cpp
+++ main.cpp
@@ -1,8 +1,144 @@
 #include <Windows.h>
+#include "WindowProcedure.h"
+#include <cstdint>
+#include <string>
+#include <format>
+#include"ConvertString.h"
+
+//directx12関係
+#include <d3d12.h>
+#include <dxgi1_6.h>
+#include <cassert>
+
+#pragma comment(lib,"d3d12.lib")
+#pragma comment(lib,"dxgi.lib")
+
+const int32_t kClientWidth = 1280;
+const int32_t kClientHeight = 720;
+
+void Log(const std::string& message)
+{
+	OutputDebugStringA(message.c_str());
+}
 
 // Windowsアプリでのエントリーポイント(main関数)
 int WINAPI WinMain(_In_ HINSTANCE, _In_opt_ HINSTANCE, _In_ LPSTR, _In_ int) {
-	OutputDebugStringA("Hello,Directx!\n");
+	
+	WNDCLASS wc{};
+	wc.lpfnWndProc = WindowProc;
+	wc.lpszClassName = L"CG2WindowClass";
+	wc.hInstance = GetModuleHandle(nullptr);
+	wc.hCursor = LoadCursor(nullptr,IDC_ARROW);
+	RegisterClass(&wc);
+
+	RECT wrc = {0,0,kClientWidth,kClientHeight};
+	AdjustWindowRect(&wrc,WS_OVERLAPPEDWINDOW,false);
+
+	HWND hwnd = CreateWindow(wc.lpszClassName,
+		L"CG2",
+		WS_OVERLAPPEDWINDOW,
+		CW_USEDEFAULT,
+		CW_USEDEFAULT,
+		wrc.right-wrc.left,
+		wrc.bottom-wrc.top,
+		nullptr,
+		nullptr,
+		wc.hInstance,
+		nullptr);
+	//ウィンドウを表示
+	ShowWindow(hwnd,SW_SHOW);
+
+
+	//DXGIファクトリーの生成
+	IDXGIFactory7* dxgiFactory = nullptr;
+
+	HRESULT hr = CreateDXGIFactory(IID_PPV_ARGS(&dxgiFactory));
+	assert(SUCCEEDED(hr));
+
+	//アダプタ関係
+	IDXGIAdapter4* useAdapter = nullptr;
+	for (UINT i = 0; dxgiFactory->EnumAdapterByGpuPreference(i, DXGI_GPU_PREFERENCE_HIGH_PERFORMANCE, IID_PPV_ARGS(&useAdapter)) !=
+		DXGI_ERROR_NOT_FOUND; i++)
+	{
+		DXGI_ADAPTER_DESC3 adapterDesc{};
+		hr = useAdapter->GetDesc3(&adapterDesc);
+		assert(SUCCEEDED(hr));
+		//ソフトウェアアダプタで無ければ
+		if (!(adapterDesc.Flags & DXGI_ADAPTER_FLAG3_SOFTWARE))
+		{
+			Log(ConvertString(std::format(L"USE Adapter:{}\n", adapterDesc.Description)));
+			break;
+		}
+		useAdapter = nullptr;
+	}
+	assert(useAdapter != nullptr);
+
+	//D3D12Deviceの生成
+	ID3D12Device* device = nullptr;
+	D3D_FEATURE_LEVEL featureLevels[] = {D3D_FEATURE_LEVEL_12_2,D3D_FEATURE_LEVEL_12_1,D3D_FEATURE_LEVEL_12_0};
+	const char* featureLevelStrings[] = {"12.2","12.1","12.0"};
+	for (size_t i=0;i<_countof(featureLevels);++i)
+	{
+		hr = D3D12CreateDevice(useAdapter,featureLevels[i],IID_PPV_ARGS(&device));
+		if (SUCCEEDED(hr))
+		{
+			Log(std::format("FeatureLevel:{}\n", featureLevelStrings[i]));
+			break;
+		}
+	}
+	assert(device != nullptr);
+	Log("Complete create D3D12Device!!!\n");
+
+	//コマンドキューの生成
+	ID3D12CommandQueue* commandQueue = nullptr;
+	D3D12_COMMAND_QUEUE_DESC commandQueueDesc{};
+	hr = device->CreateCommandQueue(&commandQueueDesc,IID_PPV_ARGS(&commandQueue));
+	//コマンドキューの生成に失敗
+	assert(SUCCEEDED(hr));
+
+	//コマンドアロケータの生成
+	ID3D12CommandAllocator* commandArocator = nullptr;
+	hr = device->CreateCommandAllocator(D3D12_COMMAND_LIST_TYPE_DIRECT,IID_PPV_ARGS(&commandArocator));
+	//コマンドアロケータの生成に失敗
+	assert(SUCCEEDED(hr));
+
+	//コマンドリストの生成
+	ID3D12CommandList* commandList = nullptr;
+	hr = device->CreateCommandList(0, D3D12_COMMAND_LIST_TYPE_DIRECT,commandArocator, nullptr,IID_PPV_ARGS(&commandList));
+	//コマンドリストの生成に失敗
+	assert(SUCCEEDED(hr));
+
+
+	//スワップチェーンの生成
+	IDXGISwapChain4* swapChain = nullptr;
+	DXGI_SWAP_CHAIN_DESC1 swapChainDesc{};
+	swapChainDesc.Width = kClientWidth;
+	swapChainDesc.Height = kClientHeight;
+	swapChainDesc.Format = DXGI_FORMAT_R8G8B8A8_UNORM;
+	swapChainDesc.SampleDesc.Count = 1;
+	swapChainDesc.BufferUsage = DXGI_USAGE_RENDER_TARGET_OUTPUT;
+	swapChainDesc.BufferCount = 2;
+	swapChainDesc.SwapEffect = DXGI_SWAP_EFFECT_FLIP_DISCARD;
+	hr = dxgiFactory->CreateSwapChainForHwnd(commandQueue, hwnd, &swapChainDesc, nullptr,nullptr,reinterpret_cast<IDXGISwapChain1**>(&swapChain));
+	assert(SUCCEEDED(hr));
+
+
+	MSG msg{};
+	//メインループ
+	while (msg.message != WM_QUIT)
+	{
+		if (PeekMessage(&msg, NULL, 0, 0, PM_REMOVE))
+		{
+			TranslateMessage(&msg);
+			DispatchMessage(&msg);
+		}
+		else
+		{
+			//処理
+		}
+	}
+
+	Log(ConvertString(std::format(L"WSTRING:{}\n",msg.message)));
 
 	return 0;
 }
